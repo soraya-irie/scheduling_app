73c8c7af1e3e9e35d1f8ba8ef3096b83ec16ef8d
soraya-ire <rujiangkongshi@gmail.com>
2024-04-06 13:02:07 +0900

    バリデーション

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,6 @@
 class Schedule < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :startdate, presence: true
+  validates :enddate, presence: true
+  validates :memo, length: { maximum: 500 }
 end
