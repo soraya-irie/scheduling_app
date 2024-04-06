75c58b237d0c2f6eb5662004dba47f82770b9e81
soraya-ire <rujiangkongshi@gmail.com>
2024-04-06 17:24:57 +0900

    バリデーションの強化

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,13 @@ class Schedule < ApplicationRecord
   validates :startdate, presence: true
   validates :enddate, presence: true
   validates :memo, length: { maximum: 500 }
+  validate :end_date_after_start_date
+
+  def end_date_after_start_date
+    return if enddate.blank? || startdate.blank?
+
+    if enddate < startdate
+      errors.add(:enddate, 'は開始日よりも後の日付でなければなりません')
+    end
+  end
 end
